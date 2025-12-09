7c0910b5dfbc419b9ca5167b673d916ad68df3e5
marco25222 <marcoantoniocabrera25222@gmail.com>
2025-12-09 16:02:21 -0500

    Update example3_safe.js

--- xss-examples/example3_safe.js
+++ xss-examples/example3_safe.js
@@ -1,12 +1,5 @@
-const escapeHtml = s => s
-  .replace(/&/g, '&amp;')
-  .replace(/</g, '&lt;')
-  .replace(/>/g, '&gt;')
-  .replace(/"/g, '&quot;')
-  .replace(/'/g, '&#x27;');
+app.get('/search-safe', (req, res) => {
+  const q = String(req.query.q || '');
 
-app.get('/search', (req, res) => {
-  const q = escapeHtml(req.query.q || '');
-  res.send(`<h1>Results for ${q}</h1>`);
+  res.json({ resultsFor: q });
 });
-
