337f673cea257c71849a26010468894ed1c825c3
marco25222 <marcoantoniocabrera25222@gmail.com>
2025-12-09 15:57:32 -0500

    Update example3_vuln.js

--- xss-examples/example3_vuln.js
+++ xss-examples/example3_vuln.js
@@ -1,16 +1,7 @@
-function escapeHtml(str) {
-  return String(str)
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#39;');
-}
-
 app.get('/search', (req, res) => {
   const q = req.query.q || '';
-  const safeQ = escapeHtml(q);
 
-  res.send(`<h1>Results for ${safeQ}</h1>`);
+  res.json({ resultsFor: q });
 });
+
 
